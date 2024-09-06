67dda04393435ebf00061ce0173a7b0099aac27a
el-crag <azertyggmf@outlook.com>
2024-09-06 01:10:51 -0500

    Class Security

--- web/assets/script.js
+++ web/assets/script.js
@@ -190,5 +190,104 @@ class Message {
 	}
 }
 
+class Security {
+	#privateKey
+	#publicKey
+
+	constructor(privateKey, publicKey) {
+		this.#privateKey = privateKey
+		this.#publicKey = publicKey
+	}
+
+	async generateKeyPair() {
+		try {
+			const keyPair = await crypto.subtle.generateKey(
+				{
+					name: "RSA-PSS",
+					modulusLength: 2048, // Tamaño de la clave en bits
+					publicExponent: new Uint8Array([1, 0, 1]), // 65537 en hexadecimal
+					hash: "SHA-256" // Algoritmo de hash para la firma
+				},
+				true, // Permite el uso de las claves para firma y verificación
+				["sign", "verify"] // Usos permitidos de las claves
+			);
+
+			this.#privateKey = keyPair.privateKey
+			this.#publicKey = keyPair.publicKey
+		} catch (error) {
+			console.error("Error generando el par de claves:", error)
+		}
+	}
+
+	async signMessage(message) {
+		try {
+			const encoder = new TextEncoder()
+			const data = encoder.encode(message)
+
+			const signature = await crypto.subtle.sign(
+				{
+					name: "RSA-PSS",
+					saltLength: 32 // longitud del "salt" en bytes
+				},
+				this.#privateKey,
+				data
+			);
+
+			return new Uint8Array(signature)
+		} catch (error) {
+			console.error("Error firmando el mensaje:", error)
+		}
+	}
+
+	async verifySignature(message, signature) {
+		try {
+			const encoder = new TextEncoder()
+			const data = encoder.encode(message)
+
+			const isValid = await crypto.subtle.verify(
+				{
+					name: "RSA-PSS",
+					saltLength: 32
+				},
+				this.#publicKey,
+				signature,
+				data
+			);
+
+			return isValid;
+		} catch (error) {
+			console.error("Error verificando la firma:", error)
+		}
+	}
+
+	async exportCryptoKey(key) {
+		const exported = await window.crypto.subtle.exportKey("spki", key)
+		const exportedAsString = String.fromCharCode.apply(null, new Uint8Array(exported))
+		const exportedAsBase64 = window.btoa(exportedAsString)
+		const pemExported = `-----BEGIN PUBLIC KEY-----\n${exportedAsBase64}\n-----END PUBLIC KEY-----`
+
+		return pemExported
+	}
+
+	async exportPublicKey() {
+		const exportedPublicKey = await this.exportCryptoKey(this.#publicKey)
+		return exportedPublicKey
+	}
+
+	async function testSign() {
+		const exportedPublic = await security.exportPublicKey()
+		console.log("Public:", exportedPublic)
+
+		// sign
+		const message = "Este es un mensaje de prueba."
+		const signature = await security.signMessage(message)
+		console.log("Firma generada:", Array.from(signature))
+
+		// Verify
+		const isValid = await security.verifySignature(message, signature)
+		console.log("La firma es válida:", isValid)
+	}
+}
+
 var chat = new Chat()
 chat.initDatabase()
